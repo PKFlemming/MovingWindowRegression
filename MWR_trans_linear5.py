9ba688bcb679c75d32820a2d3864135b209870b7
PKFlemming <100871573+PKFlemming@users.noreply.github.com>
2022-03-03 19:22:36 +0000

    Add files via upload

---
+++ MWR_trans_linear5.py
@@ -0,0 +1,234 @@
+# Front end of MWR. Driver code. Equivalent to GDR_2_2_clean.
+
+from MWRhelpers5 import txtToArray, makeheaders, regressout
+from math import floor
+import numpy as np
+from os.path import join
+import pandas as pd
+
+### INPUT BLOCK ###
+# enter file path info for input/output data
+inOutPath = r"D:\UKuni\3rdYr\Project\GIS_data\DissPy\Regression\resampled_asciis\snap_15_500_full"
+MWRtoPypath = r"D:\UKuni\3rdYr\Project\GIS_data\DissPy\Regression\MWRtoPy"
+filenamearray = [
+    'pv',  # original var name "PV", not "SEG". Check back-compatibility
+    # 'L10pv',
+    'GTI',
+    'aodb',
+    # 'T',
+    'slope',
+    'load',
+    'pl_dens',
+    'L10PCI',
+#     'L10TDP',
+#     'ed_rds_dv',
+    'logpop_fs20',
+    # 'logpop_fs30',
+#     'dummy_in_R1', # used to check windowing during translation from matlab code
+#     'dummy_in_R2',
+#     'dummy_in_R3'
+    ]
+
+windowtest = 0 # used to toggle values for python translation testing
+
+batchtag = "_15_500_full.txt" # invariant for each group of input files
+if windowtest == 1:
+    batchtag = '.txt'; # used to check windowing during translation from matlab code
+
+ncheckvar = 1 # which of the vars you want exported as a checkval
+               # to doublecheck the imported points are in the right place
+               # on the map. Y is 0, X1 is 1, Xn is n
+
+# path to write the data out
+outpath = r"D:\UKuni\3rdYr\Project\GIS_data\DissPy\Regression\resampled_asciis\snap_15_500_full\GDR_logpv"
+
+# first bit of the outfile name, specified by user at each run to identify output files:
+outfilestem = "linPVb_GASLPldlPcilPFS20"
+if windowtest == 1:
+    outfilestem = "WT"
+
+# outfiletag could just be .csv, or can include an additional version identifier ('b.csv'; 'version_2.csv') if running
+# slightly modified versions of the same basic regression
+outfiletag = '_py6_kk_OW_LLx.csv'
+
+# dimensions, in meters, of map cells:
+[cellx, celly] = [500,500]
+if windowtest == 1:
+    [cellx, celly] = [1000, 1000]
+
+# coords of top left corner of map (get from Arc):
+[refx, refy] = [398334.8956, 2032383.292]
+
+# we're going to create subarrays (windows) by slicing into our main arrays
+# first we specify the bandwidth (edge length) of our subarrays, in km:
+bw = 200
+# next we specify the spacing, in km, that we want between our focal points (the subarray centres). If sp = bw, there
+# will be no overlap, and each map cell will be counted only once. sp << bw -> lots of overlap -> focus will move more
+# slowly over the map, producing a more smoothly varying output.
+sp = 50
+
+if windowtest == 1:
+    bw = 4
+    sp = 4
+
+# Now we set the maximum fraction (0-1) of a subarray that can be NaN before we reject the subarray. Tried playing with
+# this but for non-log SEG 0.5 seems best. If it's higher you get centres plotted that are off the edge of the mapped data.
+# With logPV data it *needs* to be higher though- there's gaps in the data bc all 0 vals -> NoData. NaNmax = 0.6~0.7 works at
+# bw = 150. Presumably the further one zooms in the lower NaNmax can be.
+NaNmax = 0.7
+
+#### END OF USER INPUT ####
+
+## this is where the timer went; haven't included that in the translation yet
+
+#### READ BLOCK ####
+
+# txtToArray takes a text file of the type outputted by Arc's raster-to-ascii function (element delimiter = space,
+# line delimiter = newline) and turns it into an array
+# this line runs a list comprehension: it takes each file, as pointed to by inOutPath+filename+batchtag, turns it into an
+# array, and makes a list of all of the (2d) arrays
+separateArrays = [txtToArray(join(inOutPath, filename + batchtag)) for filename in filenamearray]
+# we then turn that back into a (3d) array
+YXarr = np.array(separateArrays) # debug comment: array has same dims in py and matlab
+
+# YXarr now holds all the data. YXarr has shape n by i by j, where n = nvars = number of variables (both Y and X) while
+# i and j are the dimensions of the map (in gridcells, not metres)
+
+#### RUN BLOCK ####
+# First we make the output filepath using the kilometre measures of bw, sp
+# This makes it easier to see which data is from which run
+outname = outfilestem + "_bw" + str(bw) + "s" + str(sp) + outfiletag
+outfile = join(outpath, outname)
+
+# Create a csv with this name and write out the column headers
+# makeheaders is a helper function that concatenates the variable name with
+# the statistic type (coefficient, p value etc.) to make the headers for
+# the output csv. It also writes the fixed headers for model stats.
+makeheaders(filenamearray, ncheckvar, outfile)
+
+### this (including the makeheaders helper) gets us up to line 113 in GDR_2_2_clean.m ###
+
+# next we convert bandwidth and spacing from km to number of cells:
+[bw, sp] = [val * 1000 / cellx for val in [bw, sp]]          # cellx = celly, so can use either
+                                                             # debug comment: bw and sp same as matlab
+# Next we see how many subarrays will fit into our data arrays on each axis
+# This tells how many rows and columns of subarrays we'll have
+# Any data points in a strip of width (gridX-bw)%sp cells along the right and height (gridY-bw)%sp cells along the
+# bottom of the main arrays will be ignored. If the area of mapped data were rectangular this could introduce a bias, as we
+# always have full data along the left and top and variably partial data along the right and bottom. In practice, the
+# impact of will be small, as the dataset on the map is an irregular shape, so most of the edge of the map is
+# NoData anyway. Could be slightly improved by centring.
+
+YXarrDims = YXarr.shape
+# print(f"\nprinting YXarrDims: \n{YXarrDims} \nend of YXarrDims\n")
+
+[nvars, gridY, gridX] = YXarrDims[:] # dimensions of the data arrays
+n_cols = floor((gridX - bw) / sp) + 1      # n subarrays in x direction # TODO: review this, incl. for odd bw
+n_rows = floor((gridY - bw) / sp) + 1      # n subarrays in y direction
+
+print(f"n_cols, n_rows = {n_cols, n_rows}") # debug comment: same n_cols, n_rows as matlab
+                                            # also makes sense on paper, incl. for (gridX - bw) % sp == 0
+# Note that for these n subarray calcs it's necessary first to subtract the bandwidth from the grid dimensions-
+# otherwise, when the top left of the subarray gets close to the bottom/right of the map, the bottom/right of the focal
+# window is off the edge.
+
+# Now we make counters to help us iterate TODO: are these no longer needed?
+i = 0 # counter for x direction
+j = 0 # counter for y direction
+
+# subx_, suby_, sp and bw are all numbers of grid cells on the map. Our arrays of datapoints directly reflect the grids
+# of cells, so we can now use subx_, suby_ etc. to index into these arrays as if we were moving a window around the map
+for j in range(n_rows):                      # go row by row till we hit the target number of rows TODO: n_rows+1?
+    print(f"j={j}")
+    suby_top = int(j * sp)                   # get index of top and bottom of row TODO: check for OBOEs
+    suby_bottom = int(suby_top + bw)
+
+    for i in range(n_cols):                  # go col by col up to target number of cols in row TODO: n_cols+1?
+        print(f"i={i}")
+        subx_left = int(i * sp)              # get left and right limits of column
+        subx_right = int(subx_left + bw)
+        # get subarrays from each main array:
+        subarrs = []                    # empty list
+        for n in range(nvars):
+            vararr = YXarr[n]           # get main array for 1st,2nd,...,nth variable
+            # index into it to get subarray and add that to the 3d array:
+            subarr = vararr[suby_top:suby_bottom,subx_left:subx_right]
+            # if n == 0:
+            #     outnameM = "j"+str(j)+"i"+str(i)+"_PVbar_py.csv"
+            #     outfileM = join(outpath, outnameM)
+            #     np.savetxt(outfileM, subarr, delimiter=',')
+
+            # print(f"n={n}")
+            # print(f"subs={suby_top, suby_bottom, subx_left, subx_right}")
+            subarrs.append(subarr)
+            arrOfSubarrs = np.array(subarrs)
+            if windowtest == 1:
+                print(f"printing subarray for j (row) = {j}; i (col) = {i}; nvar = {n} (matlab: {j+1},{i+1}, {n+1})")
+                print(subarr.astype(int))
+            # print(arrOfSubarrs.shape)
+
+        #### this gets us up to line 155 in GDR_2_2_clean.m ####
+
+        # Before processing the subarrays to run the regression, we check how many NoData values (-9999) there are.
+        # Because the map data is preprocessed to make all arrays perfectly coterminous, we only have to check one,
+        # except when using log SEG data- that array is more limited, as explained above, so for log runs we have to use
+        # the first (Y) subarray. Default is therefore to use the y subarray.
+        ySubarr = subarrs[0]
+        nNoData = np.count_nonzero(ySubarr == -9999) # count no. of NoData values in window (= nNoData in subarray)
+        if nNoData > bw * bw * NaNmax:               # acceptable limit
+            continue
+        # If the window was too NoData-heavy, it will now have been discounted. If it's still being processed, we know
+        # there aren't too many NoData values, but there may still be some, so we check. If we find any, we replace them
+        # with NaN in all subarrs:
+        if nNoData > 0:                       # check for NoData vals
+            for n in range(nvars):
+                subarr = subarrs[n]                              # get the nth subarray
+                subarr= np.where(subarr==-9999, np.nan, subarr)  # set -9999 vals to NaN
+                subarrs[n] = subarr                              # put the subarray back in
+
+        # Now we turn our 3d array (n by i by j) into a 2d array with one column per variable and one row per mapcell
+        # first we take each of the n 2d subarrays (i by j) and turn it into a 2d array with 1 column (ij by 1)
+        subarrs_in = [subarr.reshape(-1, 1) for subarr in subarrs]
+        # TODO: speedcheck (orig. had subarrs overwritten here, but changed this bc need to get checkval out of subarr.)
+        # might mean a lot of additional storage in memory
+        # now we stack these horizontally into a 2d array (ij by n)
+        hstackedSubarrs_in = np.hstack(subarrs_in)
+
+        # note that, bc the windows are square, i = j = bw/(linear resolution in km)
+        #### this gets us to line 191 in in GDR_2_2_clean.m ####
+
+        # now we make this a dataframe, bc currently that's what the backstep function is set up to accept as input
+        # TODO: make more flexible/consistent/simple
+        subarrsDF = pd.DataFrame(data = hstackedSubarrs_in,
+                                 columns = filenamearray)
+        # subarrsDF has been checked manually against subarrs_in in MatLab using dummy data, and is identical
+        # if windowtest == 1:
+        #     print(f"\nprinting subarrsDF for j (row) = {j}; i (col) = {i} (matlab: {j+1},{i+1}): \n{subarrsDF.astype(int)} \nend subarrsDF \n")
+        #     windowtestcsv = join(MWRtoPypath, "subarrscompare_py_1.csv")
+        #     subarrsDF.to_csv(windowtestcsv, mode='a', header=False) # used for debugging; can remove when done
+        # Now we get the lat and lon coords of the centre of our subarray so we can locate it on the map again. Mean of
+        # edge vals gets us central cell; -0.5 gets us to centre of cell; timesing by cell dims converts to distance on map.
+
+        lon = refx + (np.mean([subx_left, subx_right]) - 0.5) * cellx  # TODO: check for odd bw
+        lat = refy - (np.mean([suby_top, suby_bottom]) - 0.5) * celly
+
+        print(f"j={j}, i={i} (1-indexed j={j+1}, i={i+1})")
+
+        print(f"suby_top={suby_top}, suby_bottom={suby_bottom}. meanTopBottom={np.mean([suby_top, suby_bottom])}")
+        print(f"latToSubtract = {(np.mean([suby_top, suby_bottom])-0.5) * celly}; lat = {lat}")
+
+        print(f"subx_left={subx_left}, subx_right={subx_right}. meanLeftRight={np.mean([subx_left, subx_right])}")
+        print(f"lonToAdd = {(np.mean([subx_left, subx_right])-0.5) * cellx}; lon={lon}")
+        
+        # checkval is a reference value used during dev to check the mapped values are lining up correctly. If they are,
+        # this reference value should be the same as the original value in the layer on the map in the cell where the
+        # generated datapoint lands. Checkval = check value; checkvar = check variable
+        ref_in_sub = floor(bw/2)-1 # TODO: check for odd bw
+        subtocheck = subarrs[ncheckvar]
+        checkval = subtocheck[ref_in_sub,ref_in_sub]
+
+        # remove nan values from dataframe
+        subarrsDF = subarrsDF.dropna()      # TODO: is this ok???
+
+        # print(f"\nprinting NaNs in y (I think?) \n{y.isna().sum()} \nend of NaNs in y \n")
+        regressout(checkval, lon, lat, subarrsDF, outfile)
---
+++ MWRhelpers5.py
@@ -0,0 +1,241 @@
+import statsmodels.api as sm
+import numpy as np
+import csv
+from math import sqrt, floor
+
+def dictrounder(d, roundto):
+    if roundto == None: # if there's no rounding to apply, just return the original dict
+        outdict = d
+    else:
+        outdict = {k : f"{d[k]:.{roundto}g}" for k in d}
+    return(outdict)
+
+def getDfSegment(df, segment):
+    nRows = len(df.index)
+    firstHalfN = floor(nRows / 2)
+    secondHalfN = nRows - firstHalfN
+    if segment.lower() in ["n", "north"]:
+        dfSegment = df.head(firstHalfN)
+    elif segment.lower() in ["s", "south"]:
+        dfSegment = df.tail(secondHalfN)
+    elif segment.lower() in ["a", "all"]:
+        dfSegment = df                      # this shouldn't come up bc if segment == all we don't need to segment,
+                                            # but just in case...
+    print(f"using {segment} segment of data")
+    return dfSegment
+
+# to make this usable for full, segment and MW regression, it needs to take df, not path+x+y+segment.
+# assume first column is y
+# segment df before inputting, if needed
+def backStep(y, X, thold=0.05, roundto=None,verbosity=None):
+    # roundto sets rounding for display of coefs and pvals. If roundto == None, no rounding is applied
+    # rounding is done with string formatting, so rounded vals will display as 'strings'
+
+    # df = pd.read_csv(dataPath)
+    # if segment.lower() in ["n", "s", "north", "south"]:
+    #     df = getDfSegment(df, segment)
+
+    varsToRemove = True # we assume there are some variables whose pvals > thold (up until we see that there are not)
+    anyVarsRemoved = False      # to enable us to carry things over between loops we initialise this marker
+                                # we'll flip it if we remove some variables
+    xsdictToCarryOver = {}      # These are the dicts we'll use to carry things over between loops
+    xsdictCarriedOver = {}
+    while varsToRemove:
+        Xwconst = sm.add_constant(X) # TODO: this goes above loop? No, bc needs to change each time as xs removed?
+        # adds a constant (column of 1s) to the array of xs
+        xvars = list(X.columns)      # make list of X column names (x vars)
+        xswconst = ["const"] + xvars # prepend "const" to list of var names
+
+        OLSregression = sm.OLS(y, Xwconst) # these two lines run the OLS regression and get the results
+        OLSresults = OLSregression.fit()
+
+        # get the coefficients, standard errors, t statistics and pvalues out of the OLS results
+        bs, ses, ts, pvals = OLSresults.params, OLSresults.bse, OLSresults.tvalues, OLSresults.pvalues
+        if verbosity == "verbose":
+            print(OLSresults.summary())
+
+        if anyVarsRemoved == True:
+            xsdict = xsdictCarriedOver
+            # iff we're on nth loop, n>1 (1-indexed), we've removed some vars, and anyVarsRemoved == True. This means
+            # that we've removed at least one xvar from the input to the regression, but we still want to know the pval
+            # for that xvar in the regression just to confirm that it's been removed intentionally. For that reason we
+            # initialise the xdict as already containing that data on the removed variable(s)- the data carried over
+            # from the previous loop. If anyVarsRemoved == False, we haven't removed any vars and are necessarily on the
+            # first loop, so we just want an empty dict as our xsdict.
+        else:
+            xsdict = {}  # initialise empty dict to hold info on x vars
+            # the keys of xsdict will be the xkeys; the values will be- for each x variable- a dict of statistics
+        for xidx, xkey in enumerate(xswconst):
+            b = bs[xidx]
+            # populate dict of info on this x var
+            xsdict[xkey] = {"b": b,                     # regression coefficient
+                            "se": ses[xidx],            # standard error
+                            "tval" : ts[xidx],          # t value
+                            "pval": pvals[xidx],
+                            }
+        if verbosity == "verbose":
+            for x in xsdict:
+                print(x, ":", dictrounder(xsdict[x],roundto))       # TODO: make (recursive?) function?
+            # print(OLSresults.summary())
+
+        # We don't want to remove the constant from our regression, even if its pvalue is high- removing it throws off
+        # everything else and gives unrealistic r values for the overall regression. For this reason we make a "pvalues
+        # without constant" object over which to iterate. Const,pvalue is the first member of pvals, so we remove it:
+        pvalsWOConst = pvals[1:]
+
+        # if any remaining pval is greater than our threshold value, we remove the x variable with the highest pvalue
+        # and go again
+        if any([pval > thold for pval in pvalsWOConst]):  # TODO: all->any; modeldict-> end?
+            for x in xsdict:
+                # print(f"printing x: {x}")
+                xparams = xsdict[x]
+                xpval = xparams["pval"]
+                if xpval == max(pvalsWOConst): # TODO: surely more efficient way to do this? "if ismax..." or sth. idx max?
+                    X = X.drop(columns=x)
+                    if verbosity == "verbose":
+                        print(f"\nremoving variable '{x}' because its pval = {round(xpval,3)} > {thold}\n")
+                    anyVarsRemoved = True
+                    # now we populate the carry-over dict with vals we want to carry over to the next loop. We're
+                    # removing an xvar bc its pval is above our threshold, so we want to know that pval, but we don't
+                    # want to retain other info on it. Therefore other dict vals are set to nan.
+                    xsdictToCarryOver[x]={"b" : np.nan,
+                                          "se" : np.nan,
+                                          "tval" : np.nan,
+                                          "pval" : xpval}
+                    xsdictCarriedOver = xsdictToCarryOver # could be condensed, but this makes the var names more explicit
+        # if no pvals are greater than threshold, there are no variables to remove and the bs, pvals etc. we've generated
+        # are our final model params
+        else:
+            varsToRemove = False
+            modeldict = {"r2": OLSresults.rsquared,
+                         "r2adj": OLSresults.rsquared_adj,
+                         "fstat": OLSresults.fvalue,
+                         "pval": OLSresults.f_pvalue,
+                         "rmse": sqrt(OLSresults.mse_resid),
+                         # of mse_model, _total, and _resid, this is the one that matches the MatLab output
+                         }
+
+    return(xsdict, modeldict)
+
+###
+
+def regressout(checkval, i, j, yX, outfile, thold = 0.05):
+    # get y and X variables
+    y = yX.iloc[:, 0]  # get y data from df (first column)
+    X = yX.iloc[:, 1:]
+    # run backwards stepwise regression. y is dependent variable, X is set of independent variables
+    xsdict, modeldict = backStep(y, X, thold=thold, roundto=None,verbosity="None") #TODO: add toggle in driver?
+
+    # Calculate how many columns we need to have for the data we're going to write out:
+    nxs = len(X.columns)
+    ncols_out = 11 + 8 * nxs # 11 cols of model data + 8 cols for each x var
+
+    # Make array with this many columns (each window will be described by one row in the output csv)
+    data_out = np.empty(ncols_out)
+
+    # Write out coords and checkval
+    # Coords are in metres, calculated to coincide with Arcmap's values
+    # Grid system used here is Kalianpur 1975 (UTM 43N). See driver code for explanation of checkval
+    data_out[0] = i  # lon
+    data_out[1] = j  # lat
+    data_out[2] = checkval
+
+    ## Now we want to write out the model stats for this subarray regression, starting with the general model stats.
+    ## We'll do stats for each independent variable later.
+    data_out[3] = modeldict["rmse"]
+    data_out[4] = modeldict["r2"]
+    data_out[5] = modeldict["r2adj"]
+    data_out[6] = modeldict["fstat"]
+    data_out[7] = modeldict["pval"]
+    data_out[8] = np.nan                        # SSresids goes here
+
+    # Calculate and write out mean and stdev of y data
+    data_out[9] = np.nanmean(y)
+    print(y)
+    print(np.nanmean(y))
+    print(np.mean(y))
+    stdevY = np.nanstd(y, ddof=1)
+    # ddof is degrees of freedom. ddof=1 sets divisor to N-1 in stdev formula, as appropriate for stdev of a sample
+    # rather than a whole population. In practice the difference is negligible: bandwidth used was generally of the
+    # order of 100, meaning N >> 1000. At N = 1000, 1 > stdevP/stdevS > 0.999
+    data_out[10] = stdevY
+
+    # Now we loop through the model info for the independent variables
+    # For the nth X variable, we want the nth member of each array of model stats
+    xvars = list(X.columns)
+    # print(f"xvars (X.columns) = {xvars}")
+
+    for n in range(nxs):
+        # cols 0-10 are holding general model info, so we start writing at column 11
+        N = 11 + 8 * n
+        xvals = X.iloc[:, n]                # x_n
+        xvar = xvars[n]
+        # print(f"xvar = {xvar}")
+        xdict = xsdict[xvar]
+        # print(f"xdict = {xdict}")
+        b, se, tval, pval = xdict["b"], xdict["se"], xdict["tval"], xdict["pval"]
+        stdevX = np.nanstd(xvals, ddof=1)
+
+        data_out[N] = np.nanmean(xvals)  # line 74 of regressout_clean.m
+        data_out[N+1] = stdevX
+        data_out[N+2] = b           # estimated coefficient
+        data_out[N+3] = se          # standard error
+        data_out[N+4] = tval        # T statistic
+        data_out[N+5] = pval
+        bstar = b * stdevX/stdevY   # standardised regression coefficient
+        data_out[N+6] = bstar
+        data_out[N+7] = abs(bstar)     # abs of b* for comparing magnitudes
+
+    # write data out to csv
+    with open(outfile, 'a', newline='') as csvfile:
+        rowwriter = csv.writer(csvfile, delimiter=',')
+        rowwriter.writerow(data_out)
+
+
+def txtToArray(txtfile):
+    with open(txtfile) as file_name:
+        txtAsArray = np.loadtxt(file_name)
+    return txtAsArray
+
+def makeheaders(vars, ncheckvar, outfile):
+    # Function to generate headers for regression data csv written by regressout function. It creates the file and
+    # writes the headers to the first line.
+
+    PVtype = vars[0]  # first var is dependent variable Y, (log)PV density
+    xvars = vars[1:]  # rest are independent vars X1, X2...Xn
+    nxs = len(xvars)  # number of x vars
+
+    coordheads = ["xcoord", "ycoord"]  # headers for coordinate columns
+    checkvar = vars[ncheckvar]  # get name of checkvar (check variable)
+    checkvarhead = checkvar + "_ref"  # make header for checkvar col
+
+    # statsheads are the headers for the columns that contain the general model
+    # statistics (as opposed to those that pertain to individual y and x vars)
+    statsheads = ["RMSE", "r2", "adjr2", "mod_fstat", "mod_pval", "mod_ssresid"]
+    # make headers for columns containing data on y var
+    pvmeanhead = PVtype + "_mean"
+    pvstdevhead = PVtype + "_stdev"
+    pvheads = [pvmeanhead, pvstdevhead]
+    # fixed headers are those about coords, the model, the checkval, & the dependent (PV) var- everything except x vars.
+    fixedheads = coordheads + [checkvarhead] + statsheads + pvheads # all lists except cvh, so [cvh]
+
+    # xtags are descriptive suffixes, one for each piece of informtion we're
+    # going to write out about the x vars. For each x var we take (in same
+    # order as in xtags cell array below) mean, standard deviation, estimated
+    # regression coefficient, standard error, t statistic, p value,
+    # standardised regression coefficient b*, and absolute value of b*.
+    xtags = ["_mean", "_stdevX", "_b", "_st_error", "_t_stat", "_pval", "_b_star", "_b_star_abs"]
+    nxtags = len(xtags)         # number of xtags per x var
+    xheads = []                 # empty list
+
+    for n in range(nxs):
+        xvar = xvars[n];                    # for each x var...
+        for m in range(nxtags):
+            xtag = xtags[m]                 # ...for each xtag...
+            xhead = xvar + xtag             # concatenate var + tag -> col head
+            xheads.append(xhead)            # and add it to the array of column headers
+
+    headers = fixedheads + xheads           # combine fixed headers and x var headers
+    with open(outfile, 'w', newline='') as csvfile:
+        headerwriter = csv.writer(csvfile, delimiter=',')
+        headerwriter.writerow(headers)
